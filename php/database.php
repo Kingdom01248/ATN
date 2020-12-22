76d8476cf4c9f63a71beea6e419e3f6e4afc2d73
QA <nguyen0quoc1anh248@gmail.com>
2020-12-22 16:58:38 +0700

    test

--- php/database.php
+++ php/database.php
@@ -1,12 +1,12 @@
 <?php
 // Define PostgreSQL database server connect parameters.
-define('PG_HOST', 'ec2-52-44-139-108.compute-1.amazonaws.com');
+define('PG_HOST', 'ec2-3-229-51-131.compute-1.amazonaws.com');
 define('PG_PORT', 5432);
-define('PG_DATABASE', 'd3mjek46h83nkd');
-define('PG_USER', 'dbiygnexusycpe');
-define('PG_PASSWORD', '2c2be29b8295e643ce15525eaf6c21b8642245a6a2d31da580f33daf0020b27d');
-define('PG_URI', 'postgres://dbiygnexusycpe:2c2be29b8295e643ce15525eaf6c21b8642245a6a2d31da580f33daf0020b27d@ec2-52-44-139-108.compute-1.amazonaws.com:5432/d3mjek46h83nkd');
-define('PG_Heroku CLI', 'heroku pg:psql postgresql-adjacent-52772 --app agile-spire-02688');
+define('PG_DATABASE', 'dbvn8gp09bnheg');
+define('PG_USER', 'mqeckcahvzjmff');
+define('PG_PASSWORD', 'ee1c68e4e6ae3faeb4ee85fc7d551f212dc5cad54c3a356d2387ab9f4e0bed30');
+define('PG_URI', 'postgres://mqeckcahvzjmff:ee1c68e4e6ae3faeb4ee85fc7d551f212dc5cad54c3a356d2387ab9f4e0bed30@ec2-3-229-51-131.compute-1.amazonaws.com:5432/dbvn8gp09bnheg');
+define('PG_Heroku CLI', 'heroku pg:psql postgresql-animate-46775 --app desolate-scrubland-75326');
 define('ERROR_ON_CONNECT_FAILED', 'Connection failed!');
 
 // Merge connect string and connect db server with default parameters.
