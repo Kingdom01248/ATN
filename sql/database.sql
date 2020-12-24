4bc047d5ae29a6c8d2484ff13a53c688ea5e08ae
QA <nguyen0quoc1anh248@gmail.com>
2020-12-24 15:24:29 +0700

    test

--- sql/database.sql
+++ sql/database.sql
@@ -37,6 +37,54 @@ INSERT INTO product VALUES (1,
                             '',
                             'China');
 
+
 INSERT INTO product_subcategory VALUES (4, 1);
 INSERT INTO product_subcategory VALUES (5, 1);
 INSERT INTO product_subcategory VALUES (10, 1);
+INSERT INTO product VALUES (2,
+                            'PlayStation 4 500GB Black Console',
+                            'The PlayStation 4 500GB Console features a sleeker, 
+                             lighter design packed with all the power and features of PS4.
+                             High Dynamic Range
+                             Experience stunning detail and luminous colours on PS4 with HDR technology. 
+                             The contrast between colours is enhanced to bring in-game graphics closer to the world seen by the human eye.
+                             Share, 
+                             Connect & Play
+                             The intuitive PS4 interface brings you faster ways to share screenshots and video clips, 
+                             watch live streams and discover innovative ways to play.
+                             Remote Play
+                             Stream and play PS4 games on any compatible device connected to your broadband. 
+                             Switch between your phone, tablet, TV and even PC.
+                             Share Play
+                             Invite any friend who owns a PlayStation 4 console to watch you play, 
+                             take over the controls or jump into a local multiplayer game – even if they do not have a copy of the game themselves.
+                             The PlayStation 4 500GB Console in Black comes with a DUALSHOCK 4 Wireless Controller, mono headset, HDMI cable, USB and power cables. 
+                             Join online multiplayer matches, compete in tournaments and more with PlayStation Plus (sold separately).',
+                            210000,
+                            'https://image.smythstoys.com/original/desktop/155379.jpg',
+                            'China');
+
+
+INSERT INTO product_subcategory VALUES (4, 2);
+INSERT INTO product_subcategory VALUES (5, 2);
+INSERT INTO product_subcategory VALUES (11, 2);
+
+INSERT INTO product VALUES (3,
+                            'Barbie Fresh n Fun Food Truck Playset',
+                            'Beep, beep! Make way for the Barbie Fresh Fun Food Truck and a world of cooking fun. 
+                             If you love making delicious food, then you can run a food truck! Serve up salads, burgers and tacos, 
+                             and help your Barbie doll ring them up at the register.
+                             The Barbie Fresh Fun Food Truck features a stove and griddle, a smoothie station, 
+                             serving bar and storage cupboards for all your food prep essentials. 
+                             Over 30 storytelling accessories are included to spark your childs imagination. 
+                             Some even have a clip-on feature that lets Barbie dolls really hold them.
+                             Play out Barbie food truck stories with a free-standing menu board, pots, utensils, serving trays, plates, a fryer for French fries, 
+                             smoothie blender, food items, condiments and seating for your customers.',
+                            260000,
+                            'https://image.smythstoys.com/original/desktop/189321.jpg',
+                            'China');
+
+
+INSERT INTO product_subcategory VALUES (4, 3);
+INSERT INTO product_subcategory VALUES (6, 3);
+INSERT INTO product_subcategory VALUES (10, 3);
